3336ee5adf81e11020efc2df8ca557deb3e6a98a
SBF CODE <97851111+sametberke@users.noreply.github.com>
2022-08-01 09:48:50 +0300

    Create app.js

---
+++ command/app.js
@@ -0,0 +1,74 @@
+//karışanın taaaa karışma buraya meraklı komutlar klasörüne git hadi işine
+const commands = [];
+const fs = require("node:fs");
+const path = require("node:path");
+const { REST } = require('@discordjs/rest');
+const { Collection } = require("discord.js");
+const { Routes } = require('discord-api-types/v9');
+const eventsPath = path.join(__dirname, "./events");
+const { botid , token } = require("../ayarlar.json");
+const commandsPath = path.join(__dirname, "./komutlar");
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const eventFiles = fs.readdirSync(eventsPath).filter((file) => file.endsWith(".js"));
+const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith(".js"));
+
+module.exports = (client) => {
+  client.commands = new Collection();
+  client.slashcommands = new Collection();
+    for (const file of eventFiles) {
+      const filePath = path.join(eventsPath, file);
+      const event = require(filePath);
+      if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args, client));
+      } else {
+        client.on(event.name, (...args) => event.execute(...args, client));
+      }
+    };
+
+    for (const file of commandFiles) {
+      const filePath = path.join(commandsPath, file);
+      const command = require(filePath);
+      if(command.slash) {
+        client.slashcommands.set(command.name[0], command)
+        const slashCommand = new SlashCommandBuilder()
+        .setName(command.name[0])
+        .setDescription(command.description)
+        if(command.option) {
+          for(i = 0; i < command.option.length; i++) {
+            if(!command.option[i].choices) {
+              if(command.option[i].type === 'STRING') slashCommand.addStringOption(option => option.setName(command.option[i].name).setDescription(command.option[i].description).setRequired(command.option[i].require))
+              if(command.option[i].type === 'INTEGER') slashCommand.addIntegerOption(option => option.setName(command.option[i].name).setDescription(command.option[i].description).setRequired(command.option[i].require))
+              if(command.option[i].type === 'NUMBER') slashCommand.addNumberOption(option => option.setName(command.option[i].name).setDescription(command.option[i].description).setRequired(command.option[i].require))
+              if(command.option[i].type === 'BOOOLEAM') slashCommand.addBooleanOption(option => option.setName(command.option[i].name).setDescription(command.option[i].description).setRequired(command.option[i].require))
+              if(command.option[i].type === 'USER') slashCommand.addUserOption(option => option.setName(command.option[i].name).setDescription(command.option[i].description).setRequired(command.option[i].require))
+              if(command.option[i].type === 'CHANNEL') slashCommand.addChannelOption(option => option.setName(command.option[i].name).setDescription(command.option[i].description).setRequired(command.option[i].require))
+              if(command.option[i].type === 'ROLE') slashCommand.addRoleOption(option => option.setName(command.option[i].name).setDescription(command.option[i].description).setRequired(command.option[i].require))
+              if(command.option[i].type === 'MENTIONABLE') slashCommand.addMentionableOption(option => option.setName(command.option[i].name).setDescription(command.option[i].description).setRequired(command.option[i].require))
+            } else {
+              if(command.option[i].type === 'STRING') slashCommand.addStringOption(option => option.setName(command.option[i].name).setDescription(command.option[i].description).setRequired(command.option[i].require).addChoices(...command.option[i].choices))
+              if(command.option[i].type === 'INTEGER') slashCommand.addIntegerOption(option => option.setName(command.option[i].name).setDescription(command.option[i].description).setRequired(command.option[i].require).addChoices(...command.option[i].choices))
+              if(command.option[i].type === 'NUMBER') slashCommand.addNumberOption(option => option.setName(command.option[i].name).setDescription(command.option[i].description).setRequired(command.option[i].require).addChoices(...command.option[i].choices))
+              if(command.option[i].type === 'BOOLEAN') slashCommand.addBooleanOption(option => option.setName(command.option[i].name).setDescription(command.option[i].description).setRequired(command.option[i].require).addChoices(...command.option[i].choices))
+              if(command.option[i].type === 'USER') slashCommand.addUserOption(option => option.setName(command.option[i].name).setDescription(command.option[i].description).setRequired(command.option[i].require).addChoices(...command.option[i].choices))
+              if(command.option[i].type === 'CHANNEL') slashCommand.addChannelOption(option => option.setName(command.option[i].name).setDescription(command.option[i].description).setRequired(command.option[i].require).addChoices(...command.option[i].choices))
+              if(command.option[i].type === 'ROLE') slashCommand.addRoleOption(option => option.setName(command.option[i].name).setDescription(command.option[i].description).setRequired(command.option[i].require).addChoices(...command.option[i].choices))
+              if(command.option[i].type === 'MENTIONABLE') slashCommand.addMentionableOption(option => option.setName(command.option[i].name).setDescription(command.option[i].description).setRequired(command.option[i].require).addChoices(...command.option[i].choices))
+            }
+            
+          }
+        }
+        commands.push(slashCommand)
+      }
+      if(!command.slash) {
+        for(i = 0; i < command.name.length; i++) {
+          client.commands.set(command.name[i], command);
+        }
+      } 
+    }
+};
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+setTimeout(async () => {
+  rest.put(Routes.applicationCommands(botid), { body: commands }).catch(console.error);
+}, 500);
